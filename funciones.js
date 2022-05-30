96c60f8608ff49441fbad1f8f156ae776b121559
Fausto Oliva <94145013+FaustoOliva@users.noreply.github.com>
2022-05-30 10:41:14 -0300

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,59 +1,50 @@
-let limite = document.getElementsByClassName("Input")
-let arrayTareas=[];
- 
-
-let agregarTarea=(titulo)=>{
-let tarea
-}
-
-let elementos = [];
-console.log(limite)
-for (let index = 0; index < limite.length; index++) {
-    elementos[index] = document.getElementById(index);
-    console.log(elementos)
-}
+let arrayTareas = [];
+let cant = 0;
 
 
-const marcar = id =>{
-    const check = document.getElementById(id);
-    labels = document.getElementsByClassName("form-check-label");
-    label[id].classList.add('incorrecto');
+const marcar = id => {
+    id = parseInt(id);
+    const check = document.getElementById(id).value;
+    cant.toString();
+    console.log(id)
+    if (check === "on") {
+        document.getElementById(id+1).style.textDecoration = 'line-through';
+    } 
+
 }
 
 const agregarItem = () => {
+    let limite = document.getElementsByClassName("Input")
+    let elementos = [];
+  
+    console.log(limite)
+    console.log(limite.length)
+    for (let index = 0; index < limite.length; index++) {
+        elementos[index] = document.getElementById(index);
+        console.log(elementos)
+        cant = cant + 1;
+    }
+
     let checkBox = document.createElement("input");
-    let text = document.createElement("label");
     checkBox.type = "checkbox";
-    checkBox.name = "Nuevo";
     checkBox.className = "Input";
-    checkBox.id = `${elementos.length}`;
-    text.textContent = document.getElementById("Input").value;
-    let t = document.createTextNode(text.value);
+    checkBox.id = `${elementos.length+1}`;
+  
+    arrayTareas.push(checkBox);
+  
+    let label = document.createElement("label");
+    label.textContent = document.getElementById("Input").value;
+    let t = document.createTextNode(label.value);
+    label.id = cant+2;
     checkBox.appendChild(t);
 
-    if (text.textContent === '') {
+    if (label.textContent === '') {
         alert("Escribí algo!");
     } else {
         document.getElementById("form").appendChild(checkBox);
-        document.getElementById("form").appendChild(text);
+        document.getElementById("form").appendChild(label);
     }
-    document.getElementById("Input").value = "";
-
-  
+    checkBox.addEventListener('click',() => {
+        marcar(cant+1)
+    });
 }
-
-
-    
-    console.log(check.value);
-    
-    
-  
-    
-
-
-
-
-
-
-
-
