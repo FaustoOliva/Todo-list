9df37493656006d2a2227376716a06eaa3ddb418
Fausto Oliva <94145013+FaustoOliva@users.noreply.github.com>
2022-05-31 19:51:39 -0300

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,50 +1,61 @@
-let arrayTareas = [];
-let cant = 0;
+let arrayWorks = [];
+let id = 0;
 
+let refresh = () => {
+    let ul = document.getElementById("listWorks")
+    document.getElementById("listWorks").innerHTML = "";
 
-const marcar = id => {
-    id = parseInt(id);
-    const check = document.getElementById(id).value;
-    cant.toString();
-    console.log(id)
-    if (check === "on") {
-        document.getElementById(id+1).style.textDecoration = 'line-through';
-    } 
-
+    for (let index = 0; index <= arrayWorks.length; index++) {
+        if (arrayWorks[index].status) {
+            ul.innerHTML += `
+                <li><input class="Input" type="checkbox" onclick="through('${arrayWorks[index].idCheck}')" id="${arrayWorks[index].idCheck}">
+                <label class="form-check-label incorrecto" for="defaultCheck1" id="${arrayWorks[index].idLabel}">
+                ${arrayWorks[index].textContent}
+                </label></input>
+                </li>
+                `
+        } else {
+            ul.innerHTML += `<li><input class="Input" type="checkbox" onclick="through('${arrayWorks[index].idCheck}')" id="${arrayWorks[index].idCheck}">
+                <label class="form-check-label" for="defaultCheck1" id="${arrayWorks[index].idLabel}">
+                ${arrayWorks[index].textContent}
+                </label></input>
+                </li>`
+        }
+    }
 }
 
-const agregarItem = () => {
-    let limite = document.getElementsByClassName("Input")
-    let elementos = [];
-  
-    console.log(limite)
-    console.log(limite.length)
-    for (let index = 0; index < limite.length; index++) {
-        elementos[index] = document.getElementById(index);
-        console.log(elementos)
-        cant = cant + 1;
-    }
+const through = (idC) => {
+    arrayWorks[idC].status = true;
+    idC = parseInt(idC) + 1;
+    document.getElementById(idC).className += " incorrecto";
+}
 
-    let checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
-    checkBox.className = "Input";
-    checkBox.id = `${elementos.length+1}`;
-  
-    arrayTareas.push(checkBox);
-  
+const addItem = () => {
     let label = document.createElement("label");
-    label.textContent = document.getElementById("Input").value;
-    let t = document.createTextNode(label.value);
-    label.id = cant+2;
-    checkBox.appendChild(t);
+    label.textContent = document.getElementById("addText").value;
 
     if (label.textContent === '') {
         alert("Escribí algo!");
     } else {
-        document.getElementById("form").appendChild(checkBox);
-        document.getElementById("form").appendChild(label);
+        let work = {
+            idCheck: id,
+            idLabel: id + 1,
+            textContent: label.textContent,
+            status: false
+        }
+        arrayWorks.push(work);
+
+        let ul = document.getElementById("listWorks");
+        ul.innerHTML += `
+        <li><input class="Input" type="checkbox" onclick="through('${work.idCheck}')" id="${work.idCheck}">
+        <label class="form-check-label" for="defaultCheck1" id="${work.idLabel}">
+        ${label.textContent}
+        </label></input>
+        </li>
+        `
+        console.log(arrayWorks)
+        id = id + 1;
+        refresh();
     }
-    checkBox.addEventListener('click',() => {
-        marcar(cant+1)
-    });
+
 }
